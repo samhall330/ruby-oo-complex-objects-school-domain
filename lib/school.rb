f00f9071524a9583046a0dd08745c913919ec467
Sam Hall <mann.samantha11@gmail.com>
2020-12-09 21:13:59 -0500

    commit tree 6d705b844abcd9289e09b3d462612d095a732bd2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require "pry"
+
+class School
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+   
+    def roster
+        @roster
+    end
+    
+    def add_student(student, grade)
+        if @roster[grade] 
+            @roster[grade] << student
+
+        else roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+    sorted = {}
+     @roster.each do | grade, students |
+    sorted[grade] = students.sort        
+        end
+    sorted
+    end
+
+end
